5604e3ef423b72016b0ec101642c423e53acb98a
Sergey Strelchenko <serdjiostrel@gmail.com>
2020-04-20 15:54:12 +0300

    Autoloader without composer

--- public/index.php
+++ public/index.php
@@ -1,10 +1,9 @@
 <?php
 
-//require_once '../vendor/autoload.php';
-//
-//use \App\Currency;
-//
-//$currency = new Currency();
-//
-//var_dump($currency);
-echo "Hello!!!";
+require_once '../vendor/autoload.php';
+
+use \App\Currency;
+
+$currency = new Currency();
+
+echo $currency->index();
