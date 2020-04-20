4c5900e33fbbe9e37198233271a0fe02894a672d
Sergey Strelchenko <serdjiostrel@gmail.com>
2020-04-21 02:57:45 +0300

    MyAutoloader WORK

--- vendor/myautoload.php
+++ vendor/myautoload.php
@@ -1,34 +1,42 @@
 <?php
 
-function my_autoload ($ClassName) {
-    require_once '../src/' . $ClassName . ".php";
+
+class MyAutoloaderClass
+{
+
+    protected array $prefixes = array();
+
+    public function addNamespace($prefix, $base_dir)
+    {
+
+        $array_path = array($prefix => $base_dir);
+        array_push($this->prefixes, $array_path);
+//        var_dump($this->prefixes);exit;
+
+    }
+
+    public function register()
+    {
+        spl_autoload_register(array($this, 'loadClass'));
+    }
+
+    public function loadClass($class)
+    {
+        $prefix = $class;
+        while (false !== $pos = strrpos($prefix, '\\')) {
+            $prefix = substr($class, 0, $pos);
+            $relative_class = substr($class, $pos);
+            $file = $this->prefixes[0][$prefix]//    Знаю, что костыль, но работает
+                    . str_replace('\\', '/', $relative_class)
+                    . '.php';
+
+                if (file_exists($file)) {
+                   require $file;
+                }
+        }
+    }
 }
-spl_autoload_register("my_autoload");
 
-
-//class MyAutoloaderClass
-//{
-//    private $prefix;
-//    private $base_dir;
-//
-//    public function __construct($prefix, $base_dir)
-//    {
-//        $this->loadClass($prefix, $base_dir);
-//    }
-//
-//    public function loadClass($class, $dir)
-//    {
-////        include $dir . $class . '.php';
-//        return $dir . $class . '.php';
-//
-//    }
-//
-////    public function register()
-////    {
-////    spl_autoload_register( 'loadClass');
-////    }
-//
-//}
-//
-//$autoloader = new MyAutoloaderClass('Currency', '../src/');
-////$autoloader->register();
+$autoloader = new MyAutoloaderClass();
+$autoloader->addNamespace('App', '../src');
+$autoloader->register();
